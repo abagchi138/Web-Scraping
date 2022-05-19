06f592bfa8cb4d418bf826c2917c9e59980b642b
abagchi138 <93165335+abagchi138@users.noreply.github.com>
2022-05-19 17:58:45 -0400

    Add files via upload

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[14]:
+# In[1]:
 
 
-from Instructions.app import scrape
 from splinter import Browser
 import pandas as pd
 from bs4 import BeautifulSoup as bs
@@ -13,142 +12,140 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 # In[2]:
-def scrape_first():
 
 
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
 
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
 
+# In[3]:
 
-    # In[3]:
 
+mars= {}
+url= "https://redplanetscience.com/"
+browser.visit(url)
 
-    mars= {}
-    url= "https://redplanetscience.com/"
-    browser.visit(url)
 
+# In[4]:
 
-    # In[4]:
 
+html= browser.html
+soup= bs(html, 'html.parser')
+element= soup.select_one('div.list_text')
+title= element.find('div', class_= 'content_title').text
+title
+paragraph= element.find('div', class_= 'article_teaser_body').text
+paragraph
 
-    html= browser.html
-    soup= bs(html, 'html.parser')
-    element= soup.select_one('div.list_text')
-    title= element.find('div', class_= 'content_title').get_text()
-    title
-    paragraph= element.find('div', class_= 'article_teaser_body').get_text()
-    paragraph
 
+# In[5]:
 
-    # In[5]:
 
+mars["news_title"]= title
+mars["news_paragraph"]= paragraph
 
-    mars["news_title"]= title
-    mars["news_paragraph"]= paragraph
 
+# In[6]:
 
-    # In[6]:
 
+url2= "https://spaceimages-mars.com/"
+browser.visit(url2)
 
-    url2= "https://spaceimages-mars.com/"
-    browser.visit(url2)
 
+# In[7]:
 
-    # In[7]:
 
 
+element= browser.find_by_tag('button')[1]
+element.click()
 
-    element= browser.find_by_tag('button')[1]
-    element.click()
 
+# In[8]:
 
-    # In[8]:
 
+html= browser.html
+soup= bs(html, 'html.parser')
+image_url= soup.find('img', class_= 'fancybox-image')['src']
+image_url
 
-    html= browser.html
-    soup= bs(html, 'html.parser')
-    image_url= soup.find('img', class_= 'fancybox-image')['src']
-    image_url
 
+# In[9]:
 
-    # In[9]:
 
+fullurl= url2+image_url
+fullurl
 
-    fullurl= url2+image_url
-    fullurl
 
+# In[10]:
 
-    # In[10]:
 
+mars['feature_image_url']= fullurl
 
-    mars['feature_image_url']= fullurl
 
+# In[11]:
 
-    # In[11]:
 
+url4= "https://marshemispheres.com/"
+browser.visit(url4)
 
-    url4= "https://marshemispheres.com/"
-    browser.visit(url4)
 
+# In[12]:
 
-    # In[12]:
 
+hemisphere_image_url= []
+imagelink= browser.find_by_css('a.product-item img')
+for link in range(len(imagelink)):
+    hemisphere={}
+    browser.find_by_css('a.product-item img')[link].click()
+    element= browser.find_by_text('Sample')['href']
+    hemisphere['image_url']= element
+    hemisphere['title']= browser.find_by_css('h2.title').text
+    hemisphere_image_url.append(hemisphere)
+    browser.back()
+    
+    
+hemisphere_image_url    
 
-    hemisphere_image_url= []
-    imagelink= browser.find_by_css('a.product-item img')
-    for link in range(len(imagelink)):
-        hemisphere={}
-        browser.find_by_css('a.product-item img')[link].click()
-        element= browser.find_by_text('Sample')['href']
-        hemisphere['image_url']= element
-        hemisphere['title']= browser.find_by_css('h2.title').text
-        hemisphere_image_url.append(hemisphere)
-        browser.back()
-        
-        
-    hemisphere_image_url    
+mars["hemispheres"]= hemisphere_image_url
 
-    mars["hemispheres"]= hemisphere_image_url
 
+# In[13]:
 
-    # In[13]:
 
+mars
 
-    mars
 
+# In[23]:
 
-    # In[23]:
 
+url3= "https://galaxyfacts-mars.com"
+df_facts= pd.read_html(url3)[0]
+df_facts
 
-    url3= "https://galaxyfacts-mars.com"
-    df_facts= pd.read_html(url3)[0]
-    df_facts
 
+# In[24]:
 
-    # In[24]:
 
+df_facts.columns= ["description", "mars", "earth"]
+df_facts
 
-    df_facts.columns= ["description", "mars", "earth"]
-    df_facts
 
+# In[25]:
 
-    # In[25]:
 
+df_facts.set_index("description", inplace= True)
+df_facts
 
-    df_facts.set_index("description", inplace= True)
-    df_facts
 
+# In[26]:
 
-    # In[26]:
 
+mars["facts"]= df_facts.to_html(classes= "table table-striped")
+mars
 
-    mars["facts"]= df_facts.to_html(classes= "table table-striped")
-    mars
 
+# In[ ]:
 
-    # In[ ]:
-    return mars
 
-if __name__ == "__main__":
-    print(scrape_first())
+
+
